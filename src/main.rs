7ff52b79c0b16559bcdda615d81ad3704488c638
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2021-07-29 19:37:46 +0200

    Fix errors in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -11,9 +11,9 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::new()
+    let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
-        :connect(&configuration.database.connection_string())
+        .connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
